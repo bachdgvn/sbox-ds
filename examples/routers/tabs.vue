a15607cd671069603665e45325d71d92ceb84958
bachdgvn <bachdgvn@gmail.com>
2020-05-22 14:29:15 +0700

    [UPDATE] update style for tabs

--- examples/routers/tabs.vue
+++ examples/routers/tabs.vue
@@ -1,22 +1,31 @@
 <template>
     <div style="height: 5000px">
-        <Tabs type="card">
-            <TabPane v-for="tab in tabs" :key="tab" :label="'标签' + tab">标签{{ tab }}</TabPane>
-            <Button @click="handleTabsAdd" size="small" slot="extra">增加</Button>
+        <Tabs>
+            <TabPane v-for="tab in tabs" :key="tab" :label="'标签' + tab" icon="facebook">Tab {{ tab }}</TabPane>
+            <Button @click="handleTabsAdd" size="small" slot="extra">Button</Button>
         </Tabs>
+
+        <Divider></Divider>
+
+        <Tabs type="card">
+            <TabPane v-for="tab in tabs" :key="tab" :label="'标签' + tab" icon="github">Tab {{ tab }}</TabPane>
+            <Button @click="handleTabsAdd" size="small" slot="extra">Button</Button>
+        </Tabs>
+
+
     </div>
 </template>
 <script>
     export default {
         data () {
             return {
-                tabs: 20
-            }
+                tabs: 100
+            };
         },
         methods: {
             handleTabsAdd () {
                 this.tabs ++;
             }
         }
-    }
+    };
 </script>
