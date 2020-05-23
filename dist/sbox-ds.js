268a2e4a4939d9fd08fe62692e12a81eb18c995d
bachdgvn <bachdgvn@gmail.com>
2020-05-23 12:13:09 +0700

    [ADD] add releas-it

--- dist/sbox-ds.js
+++ dist/sbox-ds.js
@@ -30499,7 +30499,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '0.0.1-alpha-7',
+    version: '0.0.1-alpha-8',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
