d4a0d64fa0450c56f84094024b2b714c99408c7a
bachdgvn <bachdgvn@gmail.com>
2020-05-25 14:08:37 +0700

    [UPDATE] upgrade gulp 3.9.1 -> gulp 4.0.2

--- build/build-style.js
+++ build/build-style.js
@@ -3,10 +3,11 @@ const cleanCSS = require('gulp-clean-css');
 const less = require('gulp-less');
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
+const runSequence = require('gulp4-run-sequence');
 
 // 编译less
-gulp.task('css', function () {
-    gulp.src('../src/styles/index.less')
+gulp.task('css', async () => {
+    return gulp.src('../src/styles/index.less')
         .pipe(less())
         .pipe(autoprefixer({
             browsers: ['last 2 versions', 'ie > 8']
@@ -17,11 +18,18 @@ gulp.task('css', function () {
 });
 
 // 拷贝字体文件
-gulp.task('fonts', function () {
-    gulp.src('../src/styles/common/iconfont/fonts/*.*')
-        .pipe(gulp.dest('../dist/styles/fonts'));
-    gulp.src('../src/styles/common/textfont/fonts/*.*')
+gulp.task('iconfonts',  async () => {
+    return gulp.src('../src/styles/common/iconfont/fonts/*.*')
         .pipe(gulp.dest('../dist/styles/fonts'));
 });
 
-gulp.task('default', ['css', 'fonts']);
+gulp.task('textfonts',  async () => {
+    return gulp.src('../src/styles/common/textfont/fonts/*.*')
+        .pipe(gulp.dest('../dist/styles/fonts'));
+});
+
+// gulp.task('default', ['css', 'fonts']);
+
+gulp.task('default', function(done) {
+    runSequence('css', 'iconfonts', 'textfonts', done);
+});
