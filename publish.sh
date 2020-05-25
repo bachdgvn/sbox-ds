f46ce249ec74f627913325e0b9e59c393297cadc
bachdgvn <bachdgvn@gmail.com>
2020-05-25 14:20:06 +0700

    [UPDATE] update publish sh file

--- publish.sh
+++ publish.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+VERSION='';
+re="\"(version)\": \"([^\"]*)\"";
+
+while read -r l; do
+    if [[ $l =~ $re ]]; then
+        value="${BASH_REMATCH[2]}";
+        VERSION="$value";
+    fi
+done < package.json;
+
+# Versioning
+git tag v$VERSION
+git push origin --tag
+
+# Publish package
 npm publish --access public
 
 echo "Publish finished!";
