cb12d8c3e15418d3ed7eb0869482210c5f0737bc
bachdgvn <bachdgvn@gmail.com>
2020-06-15 15:11:52 +0700

    [RELEASE] auto release v0.0.1-alpha-38

--- dist/sbox-ds.js
+++ dist/sbox-ds.js
@@ -30811,7 +30811,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '0.0.1-alpha-37',
+    version: '0.0.1-alpha-38',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
