76a7cf0ce5fa8f9f32ca798c9a573075efc71b19
bachdgvn <bachdgvn@gmail.com>
2021-08-26 15:30:08 +0700

    [RELEASE] auto release v0.0.2

--- dist/sbox-ds.js
+++ dist/sbox-ds.js
@@ -30918,7 +30918,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '0.0.1',
+    version: '0.0.2',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
